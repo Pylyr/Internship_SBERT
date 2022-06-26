0c0458f00fc15fb04a354ea5eaa1f2143a0ea296
DAYCHMAN MARK <daychman@rance.lipn.univ-paris13.fr>
2022-06-26 02:13:45 +0200

    ipynb

--- google_drive_download.py
+++ google_drive_download.py
@@ -1,10 +1,10 @@
 import gdown
 import os
-# change path to Users/markdaychman/Desktop
 
 dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(f"{dir}/files")
-url = 'https://drive.google.com/uc?id=1-O2a5aM0S763ZIIs5UujsPYdhxBY5U3S'
+# url = 'https://drive.google.com/uc?id=1-O2a5aM0S763ZIIs5UujsPYdhxBY5U3S'
+url = 'https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/'
 
 # gdown.download_folder(url)
 gdown.download(url)
